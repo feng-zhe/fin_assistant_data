e17c2f993624087471fdebeadfcdc20c10a353b7
Zhe Feng <henryzhefeng@gmail.com>
2016-07-12 18:49:01 +0800

    improve yahoo logic

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 'use strict';
 
-const yahoo = require('./lib/stock_yahoo');
+const yahoo = require('./lib/stocks_yahoo');
 
 const today = new Date();
+var symbols = ['BIDU', 'BABA', 'JD', 'GOOG', 'APPL'];
 var options = {
     symbol: 'BIDU'
 };
 
-yahoo.get_stock(options);
+yahoo.set_index(function() {
+    let symbolIndex = -1;
+    (function inner_func() {
+        symbolIndex++;
+        if (symbolIndex >= symbols.length) return;
+        let options = {
+            symbol: symbols[symbolIndex]
+        };
+        yahoo.get_stock(options, inner_func);
+    })(0);
+});
