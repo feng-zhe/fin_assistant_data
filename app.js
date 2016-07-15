ec6645b22301b6a4db93bd5f0594ade29fa70526
Zhe Feng <henryzhefeng@gmail.com>
2016-07-15 18:17:24 +0800

    temporary push

--- app.js
+++ app.js
@@ -2,18 +2,7 @@
 
 const yahoo = require('./lib/stocks_yahoo');
 
-const today = new Date();
 const symbols = ['BIDU', 'BABA', 'JD', 'GOOG', 'AAPL', 'IBM'];
 
-// update database
-yahoo.set_index(function() {
-    let symbolIndex = -1;
-    (function inner_func() {
-        symbolIndex++;
-        if (symbolIndex >= symbols.length) return;
-        let options = {
-            symbol: symbols[symbolIndex]
-        };
-        yahoo.get_stock_all(options, inner_func);
-    })();
-});
+// recreate the collection again
+yahoo.recreate(symbols);
