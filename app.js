c93bf1d2eca97133312766fffab61dc50034bcfe
Zhe Feng <henryzhefeng@gmail.com>
2016-07-29 18:05:47 +0800

    add center file

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 'use strict';
 
 const yahoo = require('./lib/stocks_yahoo');
+const center = require('./lib/center');
 
 const symbols = ['BIDU', 'BABA', 'JD', 'GOOG', 'AAPL', 'IBM'];
 
-// recreate the collection again
-yahoo.update(symbols);
+// update then call center
+yahoo.update(symbols)
+    .then(center.update)
+    .catch(function() {
+        yahoo.recreate(symbols)
+            .then(center.update)
+            .catch(function() {
+                console.log('[error]', 'recreation of yahoo failed');
+            });
+    });
